521fae0b94a47f3fef717a666ab4cb092b1f507b
Fzkkkc <ivan.boris.04@mail.ru>
2023-01-18 18:29:18 +0300

    commit 18.01.23
    добавлен двойной прыжок на персонажа, анимация прыжка. ограничение прыжка. Добавлен враг, его анимация ходьбы и патрулирование врага

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,72 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float force;
-    Rigidbody2D rb2d;
+    [Header("Health")]
+    //public int health;
 
-    // Start is called before the first frame update
+
+    private bool isGrounded;
+
+    public float speed = 0.01f;
+
+    public float rayDistance = 0.5f;
+    public float jumpForce = 2.0f;
+    public bool doubleJump = false;
+
+
+    Rigidbody2D rb2d;
+    private Animator anim;
+
+
+
+    
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+    }
+     private void FixedUpdate()
+     {
+        RaycastHit2D hit = Physics2D.Raycast(rb2d.position, Vector2.down, rayDistance, LayerMask.GetMask("Ground"));
+
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+            doubleJump = false;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb2d.velocity = Vector2.up * force;
+            if (isGrounded)
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+            else if (!doubleJump && rb2d.velocity.y < 0)
+            {
+                doubleJump = true;
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
         }
-        transform.Translate(new Vector3(0.01f, 0, 0));
+
+        //transform.Translate(new Vector3(0.01f, 0, 0));
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (isGrounded == true)
+        {
+            anim.SetBool("grounded", true);
+        }
+        else
+        {
+            anim.SetBool("grounded", false);
+        }
     }
 }
--- Assets/Scripts/cameraSpeed.cs
+++ Assets/Scripts/cameraSpeed.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class cameraSpeed : MonoBehaviour
 {
+    public float speedcam;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class cameraSpeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0.01f, 0, 0));
+        transform.Translate(Vector2.right * speedcam * Time.deltaTime);
     }
 }
